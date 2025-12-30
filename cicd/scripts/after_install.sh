ca48201377f43502dfc959fa1a58b991117027bf
Gyan Karthik Abhishek Sakibanda <abhisheksakibanda@gmail.com>
2025-12-30 01:34:57 -0800

    Add version checks for Python and pip in after_install.sh

--- cicd/scripts/after_install.sh
+++ cicd/scripts/after_install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -e
 
-pip3 install -r /home/ec2-user/app/app/requirements.txt
+which python3
+python3 --version
+pip3 --version
+
+cd /home/ec2-user/app/app/
+pip3 install -r requirements.txt
