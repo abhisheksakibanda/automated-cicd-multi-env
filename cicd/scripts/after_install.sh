0e909c9b0a5b73335d6d9f815bb1a70b0030d99e
Gyan Karthik Abhishek Sakibanda <abhisheksakibanda@gmail.com>
2025-12-29 18:20:27 -0800

    Add debug output to after_install.sh for troubleshooting

--- cicd/scripts/after_install.sh
+++ cicd/scripts/after_install.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 set -e
 
+echo "PWD:"
+pwd
+
+echo "Contents of /home/ec2-user/app:"
+ls -la /home/ec2-user/app
+
+echo "Contents of /home/ec2-user/app/app:"
+ls -la /home/ec2-user/app/app
+
 pip3 install -r /home/ec2-user/app/app/requirements.txt
