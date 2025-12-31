fedd6ee2ef345e601e7aade80f4e77ba8647c70d
Gyan Karthik Abhishek Sakibanda <abhisheksakibanda@gmail.com>
2025-12-30 17:10:28 -0800

    Add version checks for Python and pip in after_install.sh

--- cicd/scripts/after_install.sh
+++ cicd/scripts/after_install.sh
@@ -6,5 +6,9 @@ cd /home/ec2-user/app/app
 python3 -m venv venv
 source venv/bin/activate
 
+python --version
+which python
+pip --version
+
 pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
