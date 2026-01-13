6878398d598a2e44c7031b893ce2dc31808e86f6
Gyan Karthik Abhishek Sakibanda <abhisheksakibanda@gmail.com>
2026-01-12 21:09:19 -0500

    Enhance index route response with HTML formatting and update requirements.txt for package version consistency

--- app/app.py
+++ app/app.py
@@ -14,7 +14,11 @@ def health():
 
 @app.route("/")
 def index():
-    return f'Hello from the CI/CD demo app!<br /><br />Env: {APP_ENV}', 200
+    return f'''<html>
+        <body style="background-color: #2DBC83; font-family: Arial, sans-serif; text-align: center; padding: 50px;">
+            Hello from the CI/CD demo app!<br /><br />Env: {APP_ENV}
+        </body>
+    </html>''', 200
 
 
 if __name__ == "__main__":
